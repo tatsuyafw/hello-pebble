aa080d99726987b3e10a293ce92c6c2ba5d01af0
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2016-05-08 00:19:47 +0900

    Watchface basics

--- src/hello-pebble.c
+++ src/hello-pebble.c
@@ -1,36 +1,49 @@
 #include <pebble.h>
 
-Window *window;
-TextLayer *text_layer;
+static Window *s_main_window;
+static TextLayer *s_time_layer;
 
-void init() {
-  // Create the Window
-  window = window_create();
-
-  Layer *window_layer = window_get_root_layer(window);
+static void main_window_load(Window *window) {
+  // Get information about the Window
+  Layer *window_layer = window_get_root_layer(s_main_window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  // Push to the stack, animated
-  window_stack_push(window, true);
+  // Create the TextLayer with specific bounds
+  s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
 
-  // Create the TextLayer, for display at(0, 0)
-  // and with a size of 144 x 40
-  //text_layer = text_layer_create(GRect(0, 0, 144, 40));
-  text_layer = text_layer_create(GRect(20, 85, bounds.size.w, bounds.size.h));
+  // Improve the layout to be more like a watchface
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text(s_time_layer, "00:00");
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
-  // Set the text that the TextLayer will display
-  text_layer_set_text(text_layer, "Hello, Pebble!");
-
-  // Add as child layer to be included in rendering
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  // Add is as child layer to the Window's root layer
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 
-void deinit() {
-  // Destroy the Window
-  window_destroy(window);
+static void main_window_unload(Window *window) {
+  // Destroy TextLayer
+  text_layer_destroy(s_time_layer);
+}
 
-  // Destroy the TextLayer
-  text_layer_destroy(text_layer);
+static void init() {
+  // Create main Window element and assign to pointer
+  s_main_window = window_create();
+
+  // Set handlers to manage the elements inside the Window
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+      .load = main_window_load,
+      .unload = main_window_unload
+  });
+
+  // Show the Window on the watch, with animated=true
+  window_stack_push(s_main_window, true);
+}
+
+static void deinit() {
+  // Destroy the Window
+  window_destroy(s_main_window);
 }
 
 int main(void) {
