cf9927c907deccb08c2a68285cf3e834a3d587db
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2016-05-07 22:26:03 +0900

    Adjust a text position to time round

--- src/hello-pebble.c
+++ src/hello-pebble.c
@@ -7,18 +7,22 @@ void init() {
   // Create the Window
   window = window_create();
 
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
   // Push to the stack, animated
   window_stack_push(window, true);
 
   // Create the TextLayer, for display at(0, 0)
   // and with a size of 144 x 40
-  text_layer = text_layer_create(GRect(0, 0, 144, 40));
+  //text_layer = text_layer_create(GRect(0, 0, 144, 40));
+  text_layer = text_layer_create(GRect(20, 85, bounds.size.w, bounds.size.h));
 
   // Set the text that the TextLayer will display
   text_layer_set_text(text_layer, "Hello, Pebble!");
 
   // Add as child layer to be included in rendering
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
+  layer_add_child(window_layer, text_layer_get_layer(text_layer));
 }
 
 void deinit() {
