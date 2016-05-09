6f003971c9690dba634a5a4f69c83b287079ea9a
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2016-05-09 22:47:58 +0900

    Change the background color

--- src/hello-pebble.c
+++ src/hello-pebble.c
@@ -1,5 +1,7 @@
 #include <pebble.h>
 
+# define BACKGROUND_COLOR GColorBlueMoon
+
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 static GFont s_time_font;
@@ -13,8 +15,8 @@ static void main_window_load(Window *window) {
   s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
 
   // Improve the layout to be more like a watchface
-  text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_background_color(s_time_layer, BACKGROUND_COLOR);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
   text_layer_set_text(s_time_layer, "00:00");
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
@@ -57,6 +59,8 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 static void init() {
   // Create main Window element and assign to pointer
   s_main_window = window_create();
+
+  window_set_background_color(s_main_window, BACKGROUND_COLOR);
 
   // Set handlers to manage the elements inside the Window
   window_set_window_handlers(s_main_window, (WindowHandlers) {
